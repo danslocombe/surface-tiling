a595a3dc9820a885cb56e0fb49eda05e745c2744
Dan Slocombe <ds4314@ic.ac.uk>
2016-09-16 00:19:57 +0100

    Refactor into Tiler class

--- tiling.rb
+++ tiling.rb
@@ -3,8 +3,13 @@
 
 require 'chunky_png'
 require 'matrix'
+require 'digest'
 
-all_faces = []
+
+$hash_i = 0
+def get_rand sample_size, max
+  max * $hash[$hash_i, $hash_i + sample_size].hex/(16**sample_size)
+end
 
 def min(*values)
  values.min
@@ -19,23 +24,13 @@ A = Matrix[[0, 0, 0, -1],
            [0, 1, 0, 0],
            [0, 0, 1, 1]]
 
-
+def min_max_to_i min, max
+  return max - 1 if min == 0
+  return 1 + max if min == 1
+  return 5
+end
 $dirmultx = 1
 $dirmulty = 1
-$global_xo = 0
-$global_yo = 0
-
-hue = 345#rand(360)
-h1 = hue
-h2 = (hue + 360/2) % 360
-COLORS = [
-  ChunkyPNG::Color.from_hsv(0, 0, 1),
-  ChunkyPNG::Color.from_hsv(0, 0.0, 1),
-  ChunkyPNG::Color.from_hsv(h1, 0.8, 1),
-  ChunkyPNG::Color.from_hsv(h2, 0.8, 1),
-  ChunkyPNG::Color.from_hsv(h1, 0.75, 0.6),
-  ChunkyPNG::Color.from_hsv(h2, 0.75, 0.6)
-  ]
 
 class Face
   def initialize(x, axis1, axis2, parent, children)
@@ -102,77 +97,107 @@ class Face
   end
 end
 
-all_faces << Face.new(Vector[0, 0, 0, 0], 2, 3, nil, [])
+class Tiler
+  def initialize()
+    @global_xo = 0
+    @global_yo = 0
+
+    @hue = 345#rand(360)
+    @h1 = @hue
+    @h2 = (@hue + 360/2) % 360
+
+    @colors = [
+      ChunkyPNG::Color.from_hsv(0, 0, 1),
+      ChunkyPNG::Color.from_hsv(0, 0.0, 1),
+      ChunkyPNG::Color.from_hsv(@h1, 0.8, 1),
+      ChunkyPNG::Color.from_hsv(@h2, 0.8, 1),
+      ChunkyPNG::Color.from_hsv(@h1, 0.75, 0.6),
+      ChunkyPNG::Color.from_hsv(@h2, 0.75, 0.6)
+    ]
+
+    @all_faces = []
+    @all_faces << Face.new(Vector[0, 0, 0, 0], 2, 3, nil, [])
 
 
-$image_width = 2048
-$image_height = 2048
-$ox = $image_width/2
-$oy = $image_height/2
-$scale = -42
+    @image_width = 256
+    @image_height = 256
+    @ox = @image_width/2
+    @oy = @image_height/2
+    @scale = -24
 
-#e_1 = 1
-#e_2 = lambda
-#e_3 = lamdba^2
-#e_3 = lamdba^3
-#where lambda = 1.01891 + 0.602565i
-$canonical_x = [1, 1.01891, 0.675093, -0.0520420]
-$canonical_y = [0, 0.602565, 1.22792,  1.65793]
-
-def min_max_to_i min, max
-  return max - 1 if min == 0
-  return 1 + max if min == 1
-  return 5
-end
-
-def to_image_space p
-  {x: ($ox +$global_xo -$scale * (p[0] * $canonical_x[0] + p[1] * $canonical_x[1] + 
-               p[2] * $canonical_x[2] + p[3] * $canonical_x[3])),
-   y: ($oy + $global_yo + $scale * (p[0] * $canonical_y[0] + p[1] * $canonical_y[1] + 
-               p[2] * $canonical_y[2] + p[3] * $canonical_y[3]))};
-end
-
-ticks = 30
-for i in 1 .. ticks
-  print "\nTick #{i}\n"
-  new_list = []
-  all_faces.each do |face|
-    new_list << face
-    ret = face.tick
-    new_list << ret unless ret.nil?
+    #e_1 = 1
+    #e_2 = lambda
+    #e_3 = lamdba^2
+    #e_3 = lamdba^3
+    #where lambda = 1.01891 + 0.602565i
+    @canonical_x = [1, 1.01891, 0.675093, -0.0520420]
+    @canonical_y = [0, 0.602565, 1.22792,  1.65793]
   end
-  all_faces = new_list
-
-  $global_xo = 0
-  $global_yo = 0
-  p = to_image_space(all_faces[0].pos.to_a)
-  $global_xo = $ox - p[:x]
-  $global_yo = $oy - p[:y]
-
-  # Draw
-  image = ChunkyPNG::Image.new($image_width, $image_height, ChunkyPNG::Color::TRANSPARENT)
-  #image[0, 0] = ChunkyPNG::Color.rgba(255, 0,0, 128)
-
-  all_faces.each do |face|
-    p1 = to_image_space (face.pos.to_a)
-
-    p2_m = face.pos.to_a
-    p2_m[face.min_axis] += 1
-    p2 = to_image_space(p2_m) 
-
-    p3_m = face.pos.to_a
-    p3_m[face.max_axis] += 1
-    p3 = to_image_space(p3_m) 
-
-    p4_m = face.pos.to_a
-    p4_m[face.min_axis] += 1
-    p4_m[face.max_axis] += 1
-    p4 = to_image_space(p4_m) 
 
 
-    points = ChunkyPNG::Vector.new([p1, p2, p4, p3])
-    color = COLORS[min_max_to_i face.min_axis, face.max_axis]
-    image.polygon(points, ChunkyPNG::Color::TRANSPARENT, color) 
+
+  def to_image_space p
+    {x: (@ox + @global_xo -@scale * (p[0] * @canonical_x[0] + p[1] * @canonical_x[1] + 
+                 p[2] * @canonical_x[2] + p[3] * @canonical_x[3])),
+     y: (@oy + @global_yo + @scale * (p[0] * @canonical_y[0] + p[1] * @canonical_y[1] + 
+                 p[2] * @canonical_y[2] + p[3] * @canonical_y[3]))};
   end
-  image.save("out/test_tick_#{i}.png", :interlace => false)
+
+  def tile ticks, filename
+    for i in 1 .. ticks
+      print "\nTick #{i}\n"
+      new_list = []
+      @all_faces.each do |face|
+        new_list << face
+        ret = face.tick
+        new_list << ret unless ret.nil?
+      end
+      @all_faces = new_list
+
+      @global_xo = 0
+      @global_yo = 0
+      p = to_image_space(@all_faces[0].pos.to_a)
+      @global_xo = @ox - p[:x]
+      @global_yo = @oy - p[:y]
+
+    end
+    # Draw
+    image = ChunkyPNG::Image.new(@image_width, @image_height, ChunkyPNG::Color::TRANSPARENT)
+    #image[0, 0] = ChunkyPNG::Color.rgba(255, 0,0, 128)
+
+    @all_faces.each do |face|
+      p1 = to_image_space (face.pos.to_a)
+
+      p2_m = face.pos.to_a
+      p2_m[face.min_axis] += 1
+      p2 = to_image_space(p2_m) 
+
+      p3_m = face.pos.to_a
+      p3_m[face.max_axis] += 1
+      p3 = to_image_space(p3_m) 
+
+      p4_m = face.pos.to_a
+      p4_m[face.min_axis] += 1
+      p4_m[face.max_axis] += 1
+      p4 = to_image_space(p4_m) 
+
+
+      points = ChunkyPNG::Vector.new([p1, p2, p4, p3])
+      color = @colors[min_max_to_i face.min_axis, face.max_axis]
+      image.polygon(points, ChunkyPNG::Color::TRANSPARENT, color) 
+    end
+    image.save(filename, :interlace => false)
+  end
 end
+
+input = gets
+
+md5 = Digest::MD5.new
+md5 << input
+$hash = md5.hexdigest
+
+hue = get_rand 3, 360
+s1 = 0.5 + (get_rand 2, 0.5)
+#s2 = 0.5 + (s1*get_rand 2, 0.5)
+
+Tiler.new().tile(22, 'out/test1.png')
